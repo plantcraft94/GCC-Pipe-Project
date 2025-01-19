01ab040ed23617f7ac2914a2998072cc92bc893d
plantcraft94 <plantcraft60@gmail.com>
2025-01-19 23:29:58 +0700

    add a counter to the powerup

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,22 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
     string Name;
+    public GameObject PowerCounter;
+    TextMeshProUGUI textMeshProUGUI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Name = SceneManager.GetActiveScene().name;
+        if (PowerCounter != null)
+        {
+            textMeshProUGUI = PowerCounter.GetComponent<TextMeshProUGUI>();
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PowerCounter != null)
+        {
+            textMeshProUGUI.text = GameManager.PowerCount.ToString();
+
+        }
         if (Input.GetMouseButtonDown(0) && Name == "MainMenu")
         {
             SceneManager.LoadScene("Gameplay");
         }
+
     }
     public void Restart()
     {
